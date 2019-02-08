f6fe5dd0849d6496ac7871e4cd8480d729603aed
cquinterop <cristianqpineda@gmail.com>
2019-02-08 01:30:51 -0500

    Improved element creation

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,51 +1,54 @@
+const wrapper = document.querySelector('.wrapper')
+
 // DOM MANIPULATION
 
 class UI {
+    createTag(tagName, attributes, parent, content) {
+        const tag = document.createElement(tagName)
+
+        for (let [key, value] of Object.entries(attributes)) {
+            tag.setAttribute(key, value)
+            tag.textContent = content
+            parent.appendChild(tag)
+        }
+        return tag
+    }
     newCard(person) {
-        const wrapper = document.getElementById('wrapper'),
-            card = document.createElement('article'),
-            cardImg = document.createElement('img'),
-            cardName = document.createElement('h5')
-
-        wrapper.appendChild(card)
-        card.appendChild(cardImg)
-        card.appendChild(cardName)
-
-        card.setAttribute('class', 'card col-12 col-sm-6 col-md-4 col-lg-3 p-4')
-        cardImg.setAttribute('class', 'card-img-top')
-        cardName.setAttribute('class', 'text-capitalize mt-2')
-
-        cardImg.src = person.picture.large
-        cardName.textContent = `${person.name.first} ${person.name.last}`
+        const card = this.createTag('article', { 'class': 'card col-12 col-sm-6 col-md-4 col-lg-3 p-4' }, wrapper),
+        cardImg = this.createTag('img', { 'class': 'card-img-top', 'src': person.picture.large }, card),
+        cardName = this.createTag('h5', { 'class': 'text-capitalize mt-2' }, card, `${person.name.first} ${person.name.last}`)
 
         card.addEventListener('click', () => {
             this.modal(person)
         })
     }
+    getTemplate(sel, obj) {
+        let temp = document.querySelector(sel),
+            clon = temp.content.cloneNode(true)
+            document.body.appendChild(clon)
+
+        let el
+        for (let [key, value] of Object.entries(obj)) {
+            el = document.querySelector(key)
+            el.textContent = value
+        }
+        return el
+    }
     modal(person) {
         // OPEN MODAL
-        const temp = document.querySelector('template'),
-            clon = temp.content.cloneNode(true)
-        document.body.appendChild(clon)
+        this.getTemplate('.modal', {
+            '.name': `${person.name.first} ${person.name.last}`,
+            '.gender': person.gender,
+            '.age': person.dob.age,
+            '.city': person.location.city,
+            '.phone': person.phone,
+            '.email': person.email
+        })
 
-        const picture = document.getElementById('picture'),
-            name = document.getElementById('name'),
-            gender = document.getElementById('gender'),
-            age = document.getElementById('age'),
-            city = document.getElementById('city'),
-            phone = document.getElementById('phone'),
-            email = document.getElementById('email')
-
-        name.innerHTML = `${person.name.first} ${person.name.last}`
-        picture.src = person.picture.large
-        gender.innerHTML = `<strong>Gender:</strong> ${person.gender}`
-        age.innerHTML = `<strong>Age:</strong> ${person.dob.age}`
-        city.innerHTML = `<strong>City:</strong> ${person.location.city}`
-        phone.innerHTML = `<strong>Phone:</strong> ${person.phone}`
-        email.innerHTML = `<strong>Email:</strong> ${person.email}`
+        document.querySelector('.picture').src = person.picture.large
 
         // CLOSE MODAL
-        document.getElementById('close')
+        document.querySelector('.close')
             .addEventListener('click', () => {
                 document.querySelector('.overlay')
                     .remove()
@@ -80,10 +83,10 @@ window.addEventListener('load', () => {
     get_api(12)
 })
 
-document.getElementById('amount_form')
+document.querySelector('.amount_form')
     .addEventListener('submit', e => {
-        ui.clearContent(wrapper)
-        const amount = document.getElementById('amount').value
-        get_api(amount)
         e.preventDefault()
+        const amount = document.querySelector('.amount').value
+        amount > 0 ? get_api(amount) : alert('Please enter a number greater than or equal to 1')
+        ui.clearContent(wrapper)
     })
